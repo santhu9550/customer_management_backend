e7b2d2542d4e190cc5afd25fefd67fec9cce350a
Santhosh Gunda <67777176+santhu9550@users.noreply.github.com>
2021-06-09 09:41:57 +0530

    Update accessControl.js

--- src/validations/accessControl.js
+++ src/validations/accessControl.js
@@ -3,13 +3,14 @@ const AccessControl = require('accesscontrol');
 const ac = new AccessControl();
 
 // *****Manager***********//
-ac.grant('Manager').createAny('*');
-ac.grant('Manager').deleteAny('*');
-ac.grant('Manager').deleteAny('*');
-ac.grant('Manager').readAny('*');
-ac.grant('Manager').updateAny('*');
+ac.grant('Manager').createAny('Agent').createAny('Customer');
+ac.grant('Manager').deleteAny('Agent').deleteAny('Customer');
+ac.grant('Manager').deleteAny('Agent').deleteAny('Customer');
+ac.grant('Manager').readAny('Agent').readAny('Customer');
+ac.grant('Manager').updateAny('Agent').updateAny('Customer');
 
 // *****Agent***********//
 ac.grant('Agent').createAny('Customer');
+ac.grant('Agent').readAny('Customer');
 
 module.exports = ac;
